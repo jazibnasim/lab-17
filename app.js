6d05f350154e65f23574f23bb9af327073e1eae9
Jazib Nasim <jazib.nasim@gmail.com>
2016-11-29 09:15:15 -0500

    changed a few things

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = angular.module('jazibApp', []);
 app.controller('projectInfoCtrl', function($scope){
 
 $scope.projectInfo = [ {
-src: "images/madlib.png",
-description: "This is when Angular hit me and my world changed."
+img: "images/madlib.png",
+text: "This is when Angular hit me and my world changed."
 }]
 
 });
